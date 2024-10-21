1f19bfa3442b29ef7356b266a8f1f734ccf3f797
aurelien292 <9960@holbertonstudents.com>
2024-10-21 13:59:30 +0200

    read

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - 5-printf.c
+ *
+ * Description :This function prints a specific phrase to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+printf("with proper grammar, but the outcome is a piece of art,")
+return (0);
+}
