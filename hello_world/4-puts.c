b36cda5f569869746b9ec0920137f764371d1455
aurelien292 <9960@holbertonstudents.com>
2024-10-21 12:50:20 +0200

    read

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,8 @@
 #include <stdio.h>
 
-gcc -std=gnu89 4-puts.c -o puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+ return 0;
+}
