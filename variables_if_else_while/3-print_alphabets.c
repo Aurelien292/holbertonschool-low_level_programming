320e106a0f3702eea2e0e4aefcd56cddd47e2f8d
aurelien292 <9960@holbertonstudents.com>
2024-10-22 16:08:21 +0200

    3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main -3 alpha
+ *
+ *Description: prints to the standard *out *put.
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char Lettres;
+for (Lettres 'a'; Lettres <= 'z' ; Lettres++)
+{
+putchar(Lettres);
+}
+for (Lettres 'A'; Lettres <= 'Z' ; Lettres++)
+{
+putchar(Lettres);
+}
+putchar('\n');
+return (0);
+}
