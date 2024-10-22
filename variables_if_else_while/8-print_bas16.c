670677818ff398c2d32aa45374a00b2370ef2980
aurelien292 <9960@holbertonstudents.com>
2024-10-22 17:12:21 +0200

    8

---
+++ variables_if_else_while/8-print_bas16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - 8 base 16
+ *
+ *Description: prints to the standard *out *put.
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (int Num = 0; Num < 16; Num++)
+{
+printf("%x", Num);
+}
+putchar('\n')
+return (0);
+}
