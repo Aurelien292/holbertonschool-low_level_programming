222e1a17a679f0223a0f2ca8d656961897d71c65
aurelien292 <9960@holbertonstudents.com>
2024-10-22 16:32:32 +0200

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - 5 alpha
+ *
+ *Description: prints to the standard *out *put.
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int Num;
+for (Num = 0; Num <= 10; Num++)
+printf("%d", Num)
+printf('\n')
+return (0);
+}
