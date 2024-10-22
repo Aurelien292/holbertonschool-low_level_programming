b3357f2acf75c3a050d1216d8be2e8be38578052
aurelien292 <9960@holbertonstudents.com>
2024-10-22 16:43:29 +0200

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - 6 numbers
+ *
+ *Description: prints to the standard *out *put.
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int Num;
+for (Num = 0; Num < 10; Num++)
+putchar(Num);
+putchar("\n");
+return (0);
+}
