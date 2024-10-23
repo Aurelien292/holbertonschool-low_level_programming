4dcfed249bd45020182b5aace8bdca066f65e400
aurelien292 <9960@holbertonstudents.com>
2024-10-23 15:11:15 +0200

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10
+ *
+ *Return: Always 0.
+ */
+int print_alphabet_x10()
+int main(void)
+{
+int line;
+int car;
+for (line = 0; line <= 9; ++line )
+{
+for (car = 'a'; car <= 'z'; ++ch)
+_putchar(car);
+_putchar(\n);
+}
+}
+	  
