aa979e58e77e732e22e595ecb39779788fe22c3f
aurelien292 <9960@holbertonstudents.com>
2024-10-23 15:14:30 +0200

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- *print_alphabet_x10
+ *print_alphabet_x10- affiche l'alphabet 10 fois
  *
  *Return: Always 0.
  */
-int print_alphabet_x10()
+
+int print_alphabet_x10(void)
 int main(void)
 {
 int line;
 int car;
-for (line = 0; line <= 9; ++line )
+for (line = 0; line <= 9; ++line)
 {
 for (car = 'a'; car <= 'z'; ++ch)
 _putchar(car);
 _putchar(\n);
 }
 }
-	  
