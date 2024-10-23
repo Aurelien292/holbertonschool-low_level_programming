6d8af75d659b91857efb8d78f0364fd43fbdbf5c
aurelien292 <9960@holbertonstudents.com>
2024-10-23 19:20:52 +0200

    5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -15,17 +15,17 @@ int print_sign(int pos)
 {
 if (pos > 0)
 {
-_putchar("+");
+_putchar'+');
 return (1);
 }
 else if (pos == 0)
 {
-_putchar("0");
+_putchar('0');
 return (0);
 }
 else
 {
-_putchar("-");
+_putchar('-');
 return (-1);
 }
 }
