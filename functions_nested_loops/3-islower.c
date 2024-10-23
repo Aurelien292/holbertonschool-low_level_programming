cc967caf1300c4c77cbf8ab287336bbfa455c533
aurelien292 <9960@holbertonstudents.com>
2024-10-23 16:14:41 +0200

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ *_islower - fonction qui tri les minuscules
+ *@Min parametre
+ *Return: retourn 1 si c'est une minuscule autrement retourne 0
+*/
+
+int _islower(Min);
+if (Min >= 97 && Min <= 122)
+{
+return (1);
+return (0);
+}
