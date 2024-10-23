35bab28dc6b0c75b3b491644bd10455895d2ca2a
aurelien292 <9960@holbertonstudents.com>
2024-10-23 12:35:22 +0200

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,23 +3,15 @@
  *main -print 0 _putchar char c
  *
  *Description:_putchar prints to the standard *out *put.
- *
+ *@c :ecrit le caractere a afficher 
  *Return: Always 0 (Success)
  */
 int _putchar(char c);
 int main(void)
 {
 const char *put = "_putchar\n";
- while (*put)
-   {
- _putchar(*put++);
-   }
-/*_putchar("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
-*/return (0);
+while (*put)
+{
+_putchar(*put++);
 }
+return (0);
