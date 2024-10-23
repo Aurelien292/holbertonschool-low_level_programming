42628ad5bc99cf8ad2edce3f99b7bf071852fbe8
aurelien292 <9960@holbertonstudents.com>
2024-10-23 12:20:30 +0200

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -11,12 +11,12 @@ int main(void)
 {
 const char *put = "_putchar\n";
 _putchar(*put);
-_putchar("p");
+/*_putchar("p");
 _putchar("u");
 _putchar("t");
 _putchar("c");
 _putchar("h");
 _putchar("a");
 _putchar("r");
-return (0);
+*/return (0);
 }
