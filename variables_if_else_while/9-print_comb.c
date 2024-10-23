a097002a19a71ea732a7d7e5e660d32b79013ce1
aurelien292 <9960@holbertonstudents.com>
2024-10-23 10:13:43 +0200

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - 9 Patience
+ *
+ *Description: prints to the standard *out *put.
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int Num;
+{
+for (Num = 0; Num < 10; Num++)
+putchar(Num);
+putchar (',');
+putchar (' ');
+}
+putchar (\n);
+return (0);
+}
