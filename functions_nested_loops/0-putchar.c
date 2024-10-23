5aa9dbd0a4421f231b7d170143d9dc476299127f
aurelien292 <9960@holbertonstudents.com>
2024-10-23 12:37:20 +0200

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
  *main -print 0 _putchar char c
  *
  *Description:_putchar prints to the standard *out *put.
- *@c :ecrit le caractere a afficher 
+ *@c:ecrit le caractere a afficher.
  *Return: Always 0 (Success)
  */
 int _putchar(char c);
