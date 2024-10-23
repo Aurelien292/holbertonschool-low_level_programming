73d6750a546fafe1c3f4b758dc1665b891b4dd5d
aurelien292 <9960@holbertonstudents.com>
2024-10-23 11:14:15 +0200

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-
+#include <main.h>
+/**
+ *main - 0 Putchar
+ *
+ *Description: prints to the standard *out *put.
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("_putchar\n");
-
- return (0);
+return (0);
 }
