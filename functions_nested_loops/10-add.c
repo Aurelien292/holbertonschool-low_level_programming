d4cef57b65b157dd1d90696f747b46cc257f5354
aurelien292 <9960@holbertonstudents.com>
2024-10-24 15:38:36 +0200

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Adds two integers
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Description: This function takes two integers as input and returns their sum.
+ *
+ * Return: The sum of a and b
+ */
+int main(void)
+int add (int a , int b)
+{
+return (a +b);
+}
