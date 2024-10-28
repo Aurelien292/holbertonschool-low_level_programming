fdc529393a6ec24fd74e838df4b58f16be830ee7
aurelien292 <9960@holbertonstudents.com>
2024-10-28 12:34:40 +0100

    4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * void print_most_numbers - Prints the numbers from 0 to 9, followed by a new line
+ * void print_most_numbers - Prints the numbers from 0 to 9, 
+ * followed by a new line
  *
  * Description: Uses _putchar to print each number
  */
 void print_most_numbers(void)
 {
-int i =0;
+int i = 0;
 for (i = 48; i <= 57 ; i++)
 {
 if (i != 50 && i != 52)
