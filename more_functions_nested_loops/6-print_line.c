7934e31d5c46ddf8925cf2a7b52222555b00145f
aurelien292 <9960@holbertonstudents.com>
2024-10-28 18:59:33 +0100

    7

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -7,16 +7,9 @@
  */
 void print_line(int n)
 {
-if (n <= 0)
-{
-_putchar('\n');
-}
-else
-{
 for (; n > 0; n--)
 {
 _putchar('_');
 }
 _putchar('\n');
-}
 }
---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line in the terminal.
+ * @n: The number of times the character '\' should be printed.
+ *
+ * Description: If n is 0 or less, the function only prints a newline.
+ *              For positive n, it prints a diagonal line of '\' characters,
+ *              with each character preceded by an increasing number of spaces.
+ */
+void print_diagonal(int n);
+{
+int n;
+if (n <= 0)
+{
+putchar('\n');
+}
+for (int i = 0 ; i < n; i++)
+{
+for (int j = 0 ; j < i ; j++)
+{
+putchar(' ');
+}
+putchar('\\');
+putchar('\n');
+}
+}
