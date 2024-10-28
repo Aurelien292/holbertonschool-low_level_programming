910a3c16a6473ada95b0908f59d4af0b6f36437e
aurelien292 <9960@holbertonstudents.com>
2024-10-28 10:52:43 +0100

    1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isdigit - Vérifie si un chiffre es entre 0 et 9.
+ * _isdigit - Vérifie si un chiffre est entre 0 et 9.
  * @c: Le caractère à vérifier (de type int).
  *
  * Return: 1 si c est compris entre 0 et 9 , 0 sinon.
@@ -9,7 +9,7 @@
 
 int _isdigit(int c)
 {
-if (c >= 0 && c <= 9)
+if (c >= 48 && c <= 57)
 {
 return (1);
 }
