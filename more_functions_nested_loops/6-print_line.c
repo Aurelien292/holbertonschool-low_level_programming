a8c505f9b6b9918a1bce6f55c4a29c23b0b37a7f
aurelien292 <9960@holbertonstudents.com>
2024-10-28 16:23:38 +0100

    6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,15 +9,15 @@ void print_line(int n)
 {
 if (n <= 0)
 {
-putchar('\n');
+_putchar('\n');
 }
 else
 {
 for (int i = 0; i < n ; i++)
 {
-putchar('_');
+_putchar('_');
 }
-putchar('\n');
+_putchar('\n');
 }
 return (0);
 }
