b819f9d83a83a25cc4c8edbbf7c387e05c631dc5
aurelien292 <9960@holbertonstudents.com>
2024-10-28 11:04:33 +0100

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Vérifie si un caractère est un chiffre.
+ * @a: chiffre a calculer
+ * @b: chiffre a calculer
+ * Return: 1 si c est un chiffre (0-9), 0 sinon.
+ */
+
+int mul(int a, int b)
+{
+c = a * b;
+return (0);
+}
