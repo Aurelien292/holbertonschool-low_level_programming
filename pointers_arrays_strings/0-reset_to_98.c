adc585efedaf92481ae2ee01e8ad127b7f90b0f2
aurelien292 <9960@holbertonstudents.com>
2024-10-29 13:25:58 +0100

    0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * reset_to_98 - Met à jour la valeur pointée par le pointeur à 98.
+ * @n: Pointeur vers un entier.
+ *
+ * Description: Cette fonction prend un pointeur vers un entier
+ * et met à jour la valeur de l'entier pointé à 98.
+ */
 
 void reset_to_98(int *n)
 
