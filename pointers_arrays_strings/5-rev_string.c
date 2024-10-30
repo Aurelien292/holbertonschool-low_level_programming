9ee616d160b9091338f5c1809b36d40e20ab71b1
aurelien292 <9960@holbertonstudents.com>
2024-10-30 09:44:03 +0100

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rev_string - Inverse une chaîne de caractères et l'affiche.
+ * @s: Pointeur vers la chaîne de caractères à inverser.
+ *
+ * Description: Cette fonction inverse la chaîne donnée en
+ * permutant les caractères de la première moitié de la chaîne
+ * avec ceux de la seconde moitié, puis affiche la chaîne inversée.
+ */
+void rev_string(char *s)
+{
+char temp;
+int compteur = 0;
+int i;
+while (s[compteur] != '\0')
+{
+compteur++;
+}
+for (i = 0 ; i < compteur / 2; i++)
+{
+temp = s[i];
+s[i] = s[compteur - 1 - i];
+s[compteur - 1 - i] = temp;
+}
+for (i = 0; i < compteur; i++)
+_putchar (s[i]);
+_putchar('\n';
+}
