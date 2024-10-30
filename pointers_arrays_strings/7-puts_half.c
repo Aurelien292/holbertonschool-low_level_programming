44a11809217971ea393bec3d479c6d9dce3d7184
aurelien292 <9960@holbertonstudents.com>
2024-10-30 19:11:22 +0100

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,19 +1,21 @@
 #include "main.h"
-
-
 /**
  * puts_half - Affiche la seconde moitié d'une chaîne.
  * @str: Pointeur vers la chaîne de caractères à afficher.
  */
-
 void puts_half(char *str);
 {
 int Long = 0;
 while(str[Long] != '\0')
-{Long++;}
+{
+Long++;
+}
 int debut = Long / 2 ;
 if (Long != 0)
-{Debut++;}
-for {int i = Debut; i < Long; i++)
-{putchar(str[i]);
+{
+Debut++;
+}
+for (int i = Debut; i < Long; i++)
+{
+_putchar(str[i]);
 }
