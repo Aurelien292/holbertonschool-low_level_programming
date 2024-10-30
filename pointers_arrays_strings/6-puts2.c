349228c2c6858a1b652685959e7531a3ca6fb054
aurelien292 <9960@holbertonstudents.com>
2024-10-30 14:38:39 +0100

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,14 @@
  */
 void puts2(char *str)
 {
-int i;
-for (i = 0; str[i] != '\0'; i += 2)
-{
+  int i,j;
+  for (i = 0; str[i] != '\0')
+  {
+    i++;
+  }
+ for (j = 0  j < i ; i += 2)
+
+  {
 _putchar(str[i]);
 }
 _putchar('\n');
