6d83410f9c93f4238af39c3e8ed42ccc78f6bd5a
aurelien292 <9960@holbertonstudents.com>
2024-10-31 16:22:11 +0100

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,30 +10,27 @@
  */
 int _atoi(char *s)
 {
-int sign = 1;
-int nb = 0;
-int i = 0;
-while(*s == ' ')
-{
-s++;
-}
-while (*s)
-{
-if (*s == '-')
-{
-sign *= -1;
-}
-else if (isdigit(*s))
-{
-nb = s[i];
-nb *= sign;
-nb = nb * 10 + (s[i] - '0'); 
-break;
-}
-i++;
-s++;
-}
-return (nb);
+  int sign = 1;
+ int nb = 0;
+ int i = 0;
+ while (s[i] != '\0')
+   {
+     if (s[i] == ' ')
+       {
+	 i++;
+       }
+     else  if (s[i] == '-')
+       {
+	 sign = -1;
+       }
+     else if (s[i] >= 48 && s[i] <= 57)  
+       {
+	 nb = nb * 10 + (s[i] - '0'); 
+       }
+     i++;
+   }
+ nb *= sign;
+ return (nb);
 }
 
 
