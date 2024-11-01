5f346da5a09394523f56b03386b7a943f0b2980d
aurelien292 <9960@holbertonstudents.com>
2024-11-01 16:00:37 +0100

    3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,34 +1,30 @@
 #include "main.h"
 
-
 /**
  * _strcmp - Compare deux chaînes de caractères.
- * @s1: Pointeur vers la première chaîne.
- * @s2: Pointeur vers la seconde chaîne.
+ * @s1: Pointeur vers la première chaîne de caractères.
+ * @s2: Pointeur vers la seconde chaîne de caractères.
  *
- * Cette fonction compare les chaînes de caractères s1 et s2.
+ * Cette fonction compare les chaînes s1 et s2 caractère par caractère.
  * Elle retourne un entier négatif si s1 est inférieure à s2,
  * zéro si elles sont égales, et un entier positif si s1 est
  * supérieure à s2.
  *
- * Return: La différence entre les longueurs de s1 et s2.
+ * Return: La différence entre le premier caractère non identique de
+ * s1 et s2, ou la différence de fin de chaîne si l'une des chaînes
+ * est plus courte.
  */
 
 int _strcmp(char *s1, char *s2)
 {
 int index = 0;
-int Long_s1 = 0;
-while (s1[Long_s1])
+while (s1[index] != '\0' && s2[index] != '\0')
 {
-Long_s1++;
+if (s1[index] != s2[index])
+{
+return (s1[index] - s2[index]);
 }
-while (s2[index])
-{
 index++;
 }
-if (s1 == s2)
-{
-return(0);
-}
-return (*s1 - *s2);
+return (s1[index] - s2[index]);
 }
