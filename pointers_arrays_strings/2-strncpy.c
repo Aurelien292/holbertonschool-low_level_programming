d8cfee76b69b7e4685e919a6126eb1da807c2b5c
aurelien292 <9960@holbertonstudents.com>
2024-11-01 14:21:47 +0100

    2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copie jusqu'à n caractères d'une chaîne source vers une
+ * chaîne destination.
+ *
+ * @dest: Pointeur vers la chaîne de destination qui recevra la copie.
+ * @src: Pointeur vers la chaîne source à copier.
+ * @n: Le nombre maximum de caractères à copier depuis src.
+ *
+ * Description:
+ * La fonction copie jusqu'à n caractères de la chaîne source (src) dans
+ * la chaîne de destination (dest). Si la longueur de src est
+ * inférieure à n,
+ * le reste de dest sera rempli avec des caractères nuls ('\0').
+ * Cette fonction ne garantit pas que dest est terminée par un
+ * caractère nul
+ * si n est supérieur à la longueur de src.
+ *
+ * Return: Un pointeur vers la chaîne de destination (dest).
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int index = 0;
+for (; index < n && src[index] != '\0'; index++)
+{
+dest[index] = src[index];
+}
+dest[index] = '\0';
+return (dest);
+}
