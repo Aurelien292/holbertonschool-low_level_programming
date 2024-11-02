96f598275068db762fa2606e85ccc66f891ffcfb
aurelien292 <9960@holbertonstudents.com>
2024-11-02 12:35:40 +0100

    4

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - Change toutes les lettres minuscules d'une
+ * chaîne en majuscules
+ * @str: Pointeur vers la chaîne de caractères à modifier
+ *
+ * Return: Pointeur vers la chaîne modifiée
+ */
+char *string_toupper(char *str);
+{
+int i = 0;
+while (str[i] != '\0')
+{
+if (str[i] >= 97 && str[i] <= 122)
+{
+str[i] = str[i] - 32;
+}
+i++;
+}
+return (str);
+}
