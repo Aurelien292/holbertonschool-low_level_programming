74926fc5a7cc69dd4dd017b6a9de4b82796a227e
aurelien292 <9960@holbertonstudents.com>
2024-11-03 10:38:34 +0100

    6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * cap_string - Met en majuscule tous les mots d'une chaîne.
- * @str: La chaîne à capitaliser.
+ * cap_string - Met en majuscule tous les mots d'une chaîne
+ * suivant separateur.
+ * @str: La chaîne à modifier.
  *
  * Return: Un pointeur vers la chaîne modifiée.
  */
@@ -11,26 +12,30 @@ char *cap_string(char *str)
 int i = 0;
 char Separateur[] = ",;.!?\"(){} \t\n";
 int j;
-if (str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] -= 32;
-}
-i++;
+int is_separateur;
 while (str[i] != '\0')
 {
-for (j = 0 ; Separateur[j] != '\0' ; j++)
+is_separateur = 0;
+for (j = 0; Separateur[j] != '\0'; j++)
 {
 if (str[i] == Separateur[j])
 {
+is_separateur = 1;
+break;
+}
+}
+if (is_separateur)
+{
 i++;
 if (str[i] >= 'a' && str[i] <= 'z')
 {
 str[i] -= 32;
 }
-break;
 }
-}
+else
+{
 i++;
+}
 }
 return (str);
 }
