b0201e029de25054cfe1ac7fd5cb4b65317d1a31
aurelien292 <9960@holbertonstudents.com>
2024-11-06 14:09:20 +0100

    5

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Affiche une chaîne de caractères suivie d'un saut de ligne.
+ * _puts_recursion - Affiche une chaîne de caractères suivie d'un
+ * saut de ligne.
  * @s: La chaîne de caractères à afficher.
  *
  * Description: Cette fonction affiche une chaîne de caractères en utilisant
- * la récursion. Si la chaîne n'est pas vide, elle affiche le premier caractère
+ * la récursion. Si la chaîne n'est pas vide, elle affiche
+ * le premier caractère
  * et appelle récursivement la fonction avec le reste de la chaîne. Une fois
  * que tous les caractères ont été affichés, elle affiche un saut de ligne.
  *
