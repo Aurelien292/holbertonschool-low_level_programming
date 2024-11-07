af9aad602be1091fcca05f9b5c112585826d7e0c
aurelien292 <9960@holbertonstudents.com>
2024-11-07 13:09:11 +0100

    1

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -5,19 +5,20 @@
  * @argv: Un tableau de chaînes de caractères représentant les arguments
  *
  * Description: Ce programme affiche tous les arguments passés au programme,
- * y compris le nom du programme lui-même (qui est le premier argument, argv[0]).
+ * y compris le nom du programme lui-même (qui est le premier argument,
+ * argv[0]).
  * Chaque argument est affiché sur une nouvelle ligne.
  *
- * Retour: Retourne toujours 0 pour indiquer un succès.
+ * Return: Retourne toujours 0 pour indiquer un succès.
  */
 
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 int i;
-for(i = 0; i < argc;i++)
+for (i = 0; i < argc; i++)
 {
-printf("%s\n",argv[i]);
+printf("%s\n", argv[i]);
 }
 return (0);
 }
