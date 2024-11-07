b30e04f3d9f5955d9379c4f580dbb5312d9ef715
aurelien292 <9960@holbertonstudents.com>
2024-11-07 11:30:25 +0100

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Affiche le nombre d'arguments passés au programme
+ * @argc: Le nombre d'arguments passés au programme
+ * @argv: Un tableau de chaînes de caractères représentant les arguments
+ * Description: Ce programme affiche le nombre d'arguments
+ * passés au programme,
+ * y compris le nom du programme lui-même, qui est toujours le
+ * premier argument
+ * (`argv[0]`).
+ *
+ * Return: Retourne toujours 0 pour indiquer un succès.
+ */
+int main(int __attribute__((unused))argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
