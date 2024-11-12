9f5df24290a782412b943b59a3aec229878223d5
aurelien292 <9960@holbertonstudents.com>
2024-11-12 20:55:07 +0100

    1

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -18,9 +18,11 @@ unsigned int i;
 if (size == 0)
 return (NULL);
 tableau = malloc(sizeof(char) * size);
-if (tableau == NULL)
+if (tableau == 0)
 return (NULL);
 for (i = 0; i < size; i++)
+{
 tableau[i] = c;
+} 
 return (tableau);
 }
