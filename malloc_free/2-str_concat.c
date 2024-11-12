3683b314fe895279d21dc350307af1d90df53087
aurelien292 <9960@holbertonstudents.com>
2024-11-12 20:22:45 +0100

    1

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,12 +1,30 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
-
+/**
+ * str_concat - Concatène deux chaînes de caractères.
+ * @s1: La première chaîne de caractères.
+ * @s2: La deuxième chaîne de caractères.
+ *
+ * Description:
+ * Cette fonction alloue de la mémoire et concatène les deux chaînes
+ * s1 et s2.
+ * Si l'un des arguments est `NULL`, il est traité comme une chaîne vide.
+ * La mémoire allouée est suffisante pour contenir les deux chaînes et
+ * un caractère de fin de chaîne.
+ * Si l'allocation échoue, la fonction retourne `NULL`.
+ *
+ * Return:
+ * Un pointeur vers la chaîne concaténée, ou `NULL` en cas d'échec
+ * de l'allocation.
+ */
 char *str_concat(char *s1, char *s2)
 {
 int long1 = 0;
 int long2 = 0;
 int i;
+int j;
 char *resultat;
 if (s1 == NULL)
 s1 = "";
@@ -21,8 +39,8 @@ if (resultat == NULL)
 return (NULL);
 for (i = 0; i < long1; i++)
 resultat[i] = s1[i];
-for (i = 0; i < long2; i++)
-resultat [long1 + i] = s2[i];
-resultat [long1 + long2] = '\0';
+for (j = 0; j < long2; j++)
+resultat [i + j] = s2[j];
+resultat [i + j] = '\0';
 return (resultat);
 }
