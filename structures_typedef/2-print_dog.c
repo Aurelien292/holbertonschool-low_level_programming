c06e542467fc4825cbd1ae59b9195d42e326b453
aurelien292 <9960@holbertonstudents.com>
2024-11-15 13:06:35 +0100

    bob

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -19,16 +19,19 @@ printf("\n");
 }
 if (d->name == NULL)
 {
-printf("Name: (nil)");
-}
-if (d->owner == NULL)
-{
-printf("(nil)");
+printf("Name: (nil)\n");
 }
 else
 {
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
-printf("Owner: %s\n", d->owner);
+if (d->owner == NULL)
+{
+printf("Owner: (nil)\n");
+}
+else
+{
+printf("Owner: %s\n",d->owner);
+}
 }
 }
