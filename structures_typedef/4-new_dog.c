ff55ddafc9b157a8ee7a05a15fb763f6dc48919e
aurelien292 <9960@holbertonstudents.com>
2024-11-15 15:18:42 +0100

    bob

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * new_dog - Crée une nouvelle structure dog avec des informations.
@@ -13,25 +14,50 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-struct dog *d;
-d = malloc(sizeof(struct dog));
-if (d == NULL)
-return (NULL);
-d->name = malloc(strlen(name) + 1);
-if (d->name == NULL)
-{
-free(d);
-return (NULL);
-}
-strcpy(d->name, name);
-d->age = age;
-d->owner = malloc(strlen(owner) + 1);
-if (d->owner == NULL)
-{
-free(d->name);
-free(d);
-return (NULL);
-}
-strcpy(d->owner, owner);
-return (d);
+    dog_t *d; 
+
+    
+    d = malloc(sizeof(dog_t)); 
+    if (d == NULL) 
+    {
+        return (NULL);
+    }
+
+    
+    if (name != NULL)
+    {
+        d->name = malloc(strlen(name) + 1); 
+        if (d->name == NULL)  
+        {
+            free(d);  
+            return (NULL);
+        }
+        strcpy(d->name, name); 
+    }
+    else
+    {
+        d->name = NULL;  
+    }
+
+    
+    if (owner != NULL)
+    {
+        d->owner = malloc(strlen(owner) + 1); 
+        if (d->owner == NULL)  
+        {
+            free(d->name);  
+            free(d);  
+            return (NULL);
+        }
+        strcpy(d->owner, owner);  
+    }
+    else
+    {
+        d->owner = NULL; 
+    }
+
+   
+    d->age = age;
+
+    return (d); 
 }
