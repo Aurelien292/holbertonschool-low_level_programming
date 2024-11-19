8914c34379bbfcbb005d25f7fefcdd485a66f864
aurelien292 <9960@holbertonstudents.com>
2024-11-19 09:21:18 +0100

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "function_pointers.h"
 /**
  * print_name - Affiche un nom en utilisant un pointeur de fonction
@@ -11,13 +10,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL)
-{
+if (name == NULL || f == NULL)
 return;
-}
-if (f == NULL)
-{
-return;
-}
-return (f);
+f(name);
 }
---
+++ function_pointers/3-main.c
@@ -0,0 +1,5 @@
+#include "get_op_func.c"
+#include "op_functions.c"
+#include "calc.h"
+#include <stdio.c>
+#include <stdlib.c>
