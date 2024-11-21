abaf325412307473c023c4e0647f5db7906d9ec0
aurelien292 <9960@holbertonstudents.com>
2024-11-21 14:57:42 +0100

    1

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,26 +2,21 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdarg.h>
+
 /**
- * print_all - Affiche différents types d'arguments séparés par une virgule.
- * @format: Une chaîne de caractères qui contient les types d'arguments.
- *          Chaque caractère représente un type d'argument, avec
- * les valeurs suivantes :
- *            'c' : char
- *            'i' : integer
- *            'f' : float
- *            's' : chaîne de caractères (char *)
- *          Si la chaîne de caractères est NULL, "(nil)" sera affiché
- * à la place.
- * Description :
- * La fonction prend un nombre variable d'arguments. Le format est
- * spécifié par la chaîne `format`, et chaque caractère dans cette
- * chaîne indique le type de l'argument correspondant
- * dans l'ordre. La fonction affiche les arguments séparés par une
- * virgule et un espace. Si un  argument est de type `NULL` pour
- * une chaîne de caractères, elle
- * affiche "(nil)". À la fin, un saut de ligne est ajouté pour
- * formater correctement la sortie.
+ * print_all - Affiche différents types d'arguments, séparés par une
+ * virgule.
+ * @format: Une chaîne de caractères contenant des caractères de format
+ *          spécifiant les types des arguments à afficher.
+ *          - 'c': un caractère
+ *          - 'i': un entier
+ *          - 'f': un flottant
+ *          - 's': une chaîne de caractères
+ * La fonction gère les arguments supplémentaires, les affiche en fonction de
+ * leur type, et les sépare par une virgule. Si l'argument est une chaîne de
+ * caractères et que cette chaîne est nulle, elle affiche "(nil)".
+ * La fonction ignore tout caractère dans @format qui ne correspond pas à un
+ * des types spécifiés ci-dessus.
  */
 void print_all(const char * const format, ...)
 {
