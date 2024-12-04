95de32bc0b4d39d374f9f2da294e4b07bb40a332
aurelien292 <9960@holbertonstudents.com>
2024-12-04 14:00:26 +0100

    Changement fonction print_list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -19,11 +19,11 @@ while (h != NULL)
 {
 if (h->str == NULL)
 {
-printf("[0] (nill) \n");
+printf("[0] (nil) \n");
 }
 else
 {
-printf("[%u]%s \n", h->len, h->str);
+printf("[%lu]%s \n", h->len, h->str);
 }
 count++;
 h = h->next;
