40ce57a19802c2c0f3b716ef3afa58ed1204cc07
aurelien292 <9960@holbertonstudents.com>
2024-12-08 12:10:42 +0100

    exercice 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+
+void copy_file(const char *src_file, const char *dest_file) 
+
+{
+int fd_to;
+char buffer[BUF_SIZE];
+ssize_t bytes_read;
+int fd_from = open(src_file, O_RDONLY);
+
+if (fd_from == -1)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n" , src_file);
+	exit(98);
+}
+
+fd_to = open (dest_file, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd_to == -1)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", dest_file);
+	close(fd_from);
+	exit(99);
+}
+
+while((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
+{
+	if (write(fd_to, buffer, bytes_read) != bytes_read)
+	{
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", dest_file);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+	}
+}
+if (bytes_read == -1)
+{
+	dprintf( STDERR_FILENO, "Error: Can't read from file %s\n", src_file);
+	close(fd_from);
+	close(fd_to);
+	exit(98);
+}
+if (close(fd_to) == -1)
+{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+}
+if (close(fd_to) == -1)
+{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+}
+}
+
+int main (int argc, char *argv[])
+
+{ 
+if (argc != 3)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+copy_file(argv[1], argv [2]);
+
+return 0;
+
+}
