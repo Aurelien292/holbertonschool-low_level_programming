287f1e93aeb18de22632c82fd3f2f5f2df006cd0
aurelien292 <9960@holbertonstudents.com>
2024-12-08 14:59:14 +0100

    3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,129 +2,92 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include "main.h"
+
+#define BUFFER_SIZE 1024
 
 /**
- * open_src_file - Ouvre le fichier source en lecture seule.
- * @src_file: Le fichier source à ouvrir.
- *
- * Return: le descripteur de fichier source ou termine le programme
- * avec une erreur.
+ * open_file - Ouvre un fichier en lecture ou en écriture
+ * @filename: Le nom du fichier à ouvrir
+ * @flags: Les flags d'ouverture
+ * @mode: Le mode de création (pour O_CREAT)
+ * Return: Le descripteur de fichier
  */
-int open_src_file(const char *src_file)
+int open_file(const char *filename, int flags, mode_t mode)
 {
-int fd_from = open(src_file, O_RDONLY);
-if (fd_from == -1)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src_file);
-	exit(98);
-}
-return (fd_from);
-}
+int fd = open(filename, flags, mode);
 
-/**
- * open_dest_file - Ouvre le fichier de destination en écriture,
- * le crée si nécessaire,
- *                  et le tronque.
- * @dest_file: Le fichier de destination à ouvrir.
- *
- * Return: le descripteur de fichier de destination ou termine le
- * programme avec une erreur.
- */
-int open_dest_file(const char *dest_file)
+if (fd == -1)
 {
-int fd_to;
-fd_to = open(dest_file, O_CREAT | O_WRONLY | O_TRUNC,
-S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-if (fd_to == -1)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", dest_file);
-	exit(99);
+	if (flags & O_RDONLY)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+	else
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+	exit(flags & O_RDONLY ? 98 : 99);
 }
-return (fd_to);
+return (fd);
 }
 
 /**
- * copy_data - Copie les données du fichier source vers le
- * fichier de destination.
- * @fd_from: Le descripteur du fichier source.
- * @fd_to: Le descripteur du fichier de destination.
- * @file_from: Le nom du fichier source.
- * @file_to: Le nom du fichier de destination.
- *
- * Lit les données du fichier source et les écrit dans le
- * fichier de destination.
- * En cas d'erreur de lecture ou d'écriture, affiche un
- * message d'erreur
- * approprié avec le nom du fichier concerné et termine le programme.
+ * copy_data - Copie les données d'un fichier à l'autre
+ * @fd_from: Le descripteur du fichier source
+ * @fd_to: Le descripteur du fichier de destination
+ * @src_file: Le nom du fichier source
+ * @dest_file: Le nom du fichier de destination
  */
-void copy_data(int fd_from, int fd_to, char *file_from, char *file_to)
+void copy_data(int fd_from, int fd_to, const char *src_file,
+const char *dest_file)
 {
-char buffer[1024];
+char buffer[BUFFER_SIZE];
 ssize_t bytes_read, bytes_written;
 
-while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
+while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 {
 	bytes_written = write(fd_to, buffer, bytes_read);
 	if (bytes_written != bytes_read)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest_file);
 		exit(99);
 	}
 }
-
 if (bytes_read == -1)
 {
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src_file);
 	exit(98);
 }
 }
 
 /**
- * close_files - Ferme les descripteurs de fichiers ouverts.
- * @fd_from: Le descripteur de fichier source.
- * @fd_to: Le descripteur de fichier de destination.
- *
- * Si la fermeture échoue, le programme se termine avec une erreur.
+ * close_files - Ferme les descripteurs de fichiers
+ * @fd_from: Le descripteur du fichier source
+ * @fd_to: Le descripteur du fichier de destination
  */
 void close_files(int fd_from, int fd_to)
 {
 if (close(fd_from) == -1)
-{
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-	exit(100);
-}
 if (close(fd_to) == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-exit(100);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 }
 
 /**
- * main - Fonction principale du programme.
- * @argc: Le nombre d'arguments passés au programme.
- * @argv: Le tableau des arguments passés au programme.
- *
- * Vérifie que le nombre d'arguments est correct. Si ce n'est pas le cas,
- * affiche un message d'erreur et termine avec le code 97. Sinon, appelle
- * la fonction copy_file pour copier le contenu du fichier source
- * vers le fichier de destination.
- *
- * Return: 0 si tout se passe bien.
+ * main - Point d'entrée du programme
+ * @argc: Nombre d'arguments
+ * @argv: Tableau des arguments
+ * Return: 0 en cas de succès
  */
 int main(int argc, char *argv[])
 {
-int fd_from;
-int fd_to;
+int fd_from, fd_to;
 
 if (argc != 3)
 {
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
 }
 
-fd_from = open_src_file(argv[1]);
-fd_to = open_dest_file(argv[2]);
+fd_from = open_file(argv[1], O_RDONLY, 0);
+fd_to = open_file(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
 copy_data(fd_from, fd_to, argv[1], argv[2]);
 close_files(fd_from, fd_to);
 
