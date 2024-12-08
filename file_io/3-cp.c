dd9cb8db345403aeb0b772a142b11f29a68de29b
aurelien292 <9960@holbertonstudents.com>
2024-12-08 13:12:00 +0100

    3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,9 +8,12 @@ void copy_file(const char *src_file, const char *dest_file)
 
 {
 int fd_to;
+int fd_from;
 char buffer[BUF_SIZE];
 ssize_t bytes_read;
-int fd_from = open(src_file, O_RDONLY);
+ssize_t bytes_written;
+
+fd_from = open(src_file, O_RDONLY);
 
 if (fd_from == -1)
 {
@@ -22,15 +25,18 @@ fd_to = open (dest_file, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (fd_to == -1)
 {
 	dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", dest_file);
+	printf ("erreur creation");
 	close(fd_from);
 	exit(99);
 }
 
-while((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
+while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
 {
-	if (write(fd_to, buffer, bytes_read) != bytes_read)
+	bytes_written = write(fd_to, buffer, bytes_read);
+	if (bytes_written != bytes_read)
 	{
 			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", dest_file);
+			printf ("erreur d'ecriture");
 			close(fd_from);
 			close(fd_to);
 			exit(99);
@@ -43,7 +49,7 @@ if (bytes_read == -1)
 	close(fd_to);
 	exit(98);
 }
-if (close(fd_to) == -1)
+if (close(fd_from) == -1)
 {
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
@@ -65,6 +71,6 @@ if (argc != 3)
 }
 copy_file(argv[1], argv [2]);
 
-return 0;
+return (0);
 
 }
