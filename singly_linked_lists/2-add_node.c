8053f3c8c0e3393e105b13a2fe935c5ae1c71f2f
aurelien292 <9960@holbertonstudents.com>
2025-05-09 16:35:11 +0200

    add

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,23 +2,6 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
-/**
- * add_node - Ajoute un nouveau nœud au début d'une liste_t.
- * @head: Un pointeur vers le pointeur de la tête de la liste.
- * @str: La chaîne de caractères à ajouter au nouveau nœud.
- *
- * Description: Cette fonction crée un nouveau nœud avec la chaîne `str`,
- * duplique la chaîne, puis ajoute ce nouveau nœud au début de la liste.
- * Si l'allocation mémoire échoue, la fonction renvoie NULL.
- *
- * Return: L'adresse du nouvel élément, ou NULL si l'opération échoue.
- */
-
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
 /**
  * add_node - Adds a new node at the beginning of a list_t list
  * @head: Double pointer to the head of the list
